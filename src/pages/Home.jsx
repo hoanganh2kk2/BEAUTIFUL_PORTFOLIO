a0802734fa0c0890e45f0892ae27eef718ec2f06
hoanganh2kk2 <hoanganh11042kk2@gmail.com>
2025-06-06 21:08:04 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ThemeToggle from "../component/ThemeToggle";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+
+      {/* Navbar */}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default Home;
